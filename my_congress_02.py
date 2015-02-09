a3260422527dc450f58cd4a3fd4422544d3153b9
cambyses <kambiz.behi@post.harvard.edu>
2015-02-09 16:01:43 -0600

    JSON Problems
    File my_congress_01 and its variations create errors.

---
+++ my_congress_02.py
@@ -0,0 +1,19 @@
+
+import json
+import codecs
+
+f = codecs.open("congress.json","r","utf-8")
+content = f.read()
+jsondata = json.loads(content)
+
+print("The type of the data is: ",type(jsondata["results"][0]["members"]))
+print("And it is a list of ",type(jsondata["results"][0]["members"][0]))
+
+members = set()
+
+for items in jsondata["results"][0]["members"]:
+    
+    members.add(items["id"])
+
+print("The total number of members are: ",len(members))
+    
